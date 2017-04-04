15306654cca9a7f252bee4e4e07d0fa96f1fbc30
Martijn Boers <martijn.s.boers@gmail.com>
2017-04-04 15:40:27 +0200

    Aanmaken van project kan, nog niet toevoegen actoren

--- app/src/main/java/com/gitgud/actortemplateapp/NewActorTemplateFragment.java
+++ app/src/main/java/com/gitgud/actortemplateapp/NewActorTemplateFragment.java
@@ -1,13 +1,21 @@
 package com.gitgud.actortemplateapp;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by martijn on 04/04/17.
@@ -22,7 +30,7 @@ public class NewActorTemplateFragment extends AppCompatActivity {
         setContentView(R.layout.activity_new_content);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mDatabase = FirebaseDatabase.getInstance().getReference();
     }
 
@@ -38,8 +46,38 @@ public class NewActorTemplateFragment extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.save_actor) {
-            // doe iets
-            return true;
+            try {
+                TextView naam = (TextView) findViewById(R.id.naam);
+                TextView omschrijving = (TextView) findViewById(R.id.omschrijving);
+
+                if (naam.getText().length() > 0 && omschrijving.getText().length() > 0) {
+                    ProjectEntry pj = new ProjectEntry();
+                    pj.setName(naam.getText().toString());
+                    pj.setDescription(omschrijving.getText().toString());
+                    pj.setCreatedAt(new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(Calendar.getInstance().getTime()));
+
+                    mDatabase.child("projects").push().setValue(pj);
+
+                    Snackbar.make(this.findViewById(android.R.id.content), "Nieuw project aangemaakt", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            finish();
+                        }
+                    }, 1000);
+                    return true;
+                } else {
+                    Snackbar.make(this.findViewById(android.R.id.content), "Niet alle velden zijn ingevuld", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    return false;
+                }
+            } catch (Exception e) {
+                Snackbar.make(this.findViewById(android.R.id.content), String.format("Er is iets misgegaan met het aanmaken van het project, %s", e.getMessage()), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
         }
         return super.onOptionsItemSelected(item);
     }
