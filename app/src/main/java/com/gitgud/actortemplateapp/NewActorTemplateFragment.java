e2c64b5164981f2d11e61ac08dde1ea0f7cd141c
Martijn Boers <martijn.s.boers@gmail.com>
2017-04-04 13:50:23 +0200

    Standaard icons van material.io/icons

---
+++ app/src/main/java/com/gitgud/actortemplateapp/NewActorTemplateFragment.java
@@ -0,0 +1,41 @@
+package com.gitgud.actortemplateapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by martijn on 04/04/17.
+ */
+
+public class NewActorTemplateFragment extends AppCompatActivity {
+    private DatabaseReference mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_new_actor_template);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_actor_template, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return true;
+    }
+
+}
